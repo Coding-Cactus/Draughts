5eb4797ece681896f0ad9bea1be9e5057e61ea74
raem-haq <86297821+raem-haq@users.noreply.github.com>
2022-07-27 21:47:33 +0500

    Update checkers.py

--- checkers.py
+++ checkers.py
@@ -1,39 +1,54 @@
 #CODE IS NOT FINISHED
 class Piece:
 
-    def __init__(self,player):
+    def __init__(self,player,row,col):
         self.player = player
         self.is_king = False
-        if self.player == 0:
-            self.is_piece = False
-        else:
-            self.is_piece = True
-        if self.player == 1:
-            self.movement = -1
-        else:
-            self.movement = +1
+        self.row = row
+        self.col = col
 
     def promote(self):
         self.is_king = True
+
 class Board:
     def __init__(self):
         self.board = []
-        for i in range(8):
-            self.board.append([])
-            for j in range(8):
-                self.board[i].append(Piece(0))
-        for i in range(3):
-            for j in range(i % 2, 8, 2):
-                self.board[i][j] = Piece(1)
-
-        for i in range(5,8):
-            for j in range(i % 2 - 1, 8, 2):
-                self.board[i][j] = Piece(2)
         self.play1_pieces = 12
         self.play2_pieces = 12
+        self.create_board()
 
     def show(self):
         print(self.board)
+
+    def move(self, piece, row, col):
+        self.board[row][col] = self.board[piece.row][piece.col]
+        if abs(piece.row - row) == 1 and abs(piece.col - col) == 1:
+            piece = self.board[(row+piece.row)/2][(col+piece.col)/2]
+            if piece != 0:
+                if piece.player == 1:
+                    self.play1_pieces -= 1
+                elif piece.player == 2:
+                    self.play2_pieces -= 1
+
+            self.board[(row+piece.row)/2][(col+piece.col)/2] = 0
+        self.board[piece.row][piece.col] = self.board[row][col]
+
+    def get_piece(self, row, col):
+        return self.board[row][col]
+
+    def create_board(self):
+        for row in range(8):
+            self.board.append(row)
+            for col in range(8):
+                if col % 2 == (row+1) % 2:
+                    if row < 3:
+                        self.board[row].append(Piece(row, col, 1))
+                    elif row > 4:
+                        self.board[row].append(Piece(row, col, 2))
+                    else:
+                        self.board[row].append(0)
+                else:
+                    self.board[row].append(0)
 
     def move_adjacent(self, init, final,active_player):
         selected_piece = board[init[0]][init[1]]
@@ -139,6 +154,42 @@ class Board:
                                         captures.append(((i, j), (i - 2, j + 2)))
         return captures
 
+class Game:
+    def __init__(self):
+        self.board = Board()
+        self.turn = 1
+        self.saved_piece = None
+
+    def are_pieces(self):
+        if self.turn == 1 and self.board.play1_pieces == 0:
+            return False
+        if self.turn == 2 and self.board.play2_pieces == 0:
+            return False
+        return True
+
+    def change_turn(self):
+        self.turn = 3 - self.turn
+
+    def captures(self, piece):
+        if self.board[piece.row][piece.col] != 0:
+            if self.board[piece.row+1][piece.col-1].player == 3 - self.turn:
+                if piece.player == 1 or piece.is_king:
+                    pass
+                if piece.player == 2 or piece.is_king:
+                    pass
+
+class Tree:
+    pass
+
+
+
+# need to check for all captures
+# if there are none, check if there are any available moves
+# if there are available moves, let the user select a piece and a destination
+# and see if the piece is in the list of available moves
+
+
+"""
 board_class = Board()
 
 selected_piece_location = (0,0)
@@ -162,3 +213,4 @@ while not game_over:
         while not is_valid:
             #get move_to_location and selected_piece_location from user via nathaniel
             is_valid = board_class.move(selected_piece_location,move_to_location)
+"""
