dc3b5ca572760f3c47949edb3dcdeab5f06c783f
Coding-Cactus <CodingCactus.cc@gmail.com>
2022-09-02 15:25:56 +0100

    :iphone: Make text responsive

--- lib/stylesheet.py
+++ lib/stylesheet.py
@@ -21,57 +21,64 @@ stylesheet = f"""
         min-width: 800px;
         min-height: 800px;
     }}
-    
+
     QLabel {{
         color: {var("--main-highlight")};
-        font-size: 20px;
         margin: 0px;
     }}
-    
+
     QLabel#title {{
-        font-size: 75px;
         margin-bottom: 50px;
     }}
-    
+
     QPushButton {{
-        background-repeat: no-repeat;
         background-position: center;
     }}
-        
+
     QPushButton {{
         border: 1px solid {var("--main-highlight")};
         border-radius: 5px;
-        font-size: 25px;
         color: {var("--main-highlight")};
         padding: 20px;
-        max-width: 230px;
     }}
-    
+
     QPushButton#home {{
-        background-image: {url("imgs/button.png")};
+        background-image: {url("imgs/button.jpg")};
         margin: 50px 75px;
+    }}
+
+    QPushButton#how {{
+        background-image: {url("imgs/button.jpg")};
+        margin: 50px 225px;
         width: 200px;
     }}
-    
-    QPushButton#how {{
-        background-image: {url("imgs/button.png")};
-        margin: 50px 225px;    
-        width: 200px;    
+
+    QScrollArea#content {{
+        background-color: rgba(0, 0, 0, 0.4);
+        padding: 20px;
+        border-radius: 20px;
+        max-width: 1500px;
     }}
-    
+
+    QLabel#content, QWidget#content-text {{
+        background-color: transparent;
+    }}
+
     QPushButton#tile-active {{
         padding: 0px;
         margin: 0px;
         border-radius: 0px;
         border: 1px solid {var("--main-highlight")};
         background-color: rgba(13, 13, 13, 0.75);
+        background-repeat: no-repeat;
     }}
-    
+
     QPushButton#tile-inactive {{
         padding: 0px;
         margin: 0px;
         border: none;
         border-radius: 0px;
         background-color: transparent;
+        background-repeat: no-repeat;
     }}
 """
